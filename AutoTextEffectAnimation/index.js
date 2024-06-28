0ad359565a20a93e209a7fb2b0dc3daa71cc41e3
waqar <muhammadwaqaryounas304@gmail.com>
2024-06-28 12:56:37 -0700

    fix basic bug

--- AutoTextEffectAnimation/index.js
+++ AutoTextEffectAnimation/index.js
@@ -7,7 +7,9 @@ let characterIndex = 0;
 function updateText() {
   characterIndex++;
   containerEl.innerHTML = `<h1>
-    I am a ${careers[careerIndex].slice(0, characterIndex)}</h1>`;
+    I am ${careers[careerIndex].slice(0, 1) === "I" ? "an" : "a"} ${careers[
+    careerIndex
+  ].slice(0, characterIndex)}</h1>`;
   if (characterIndex === careers[careerIndex].length) {
     careerIndex++;
     characterIndex = 0;
